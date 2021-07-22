0f428a58ce95c7337e05e060e590ca9379703fa0
Yulia <yuliagu08@gmail.com>
2021-07-22 10:35:55 -0400

    integrated picture feed into main storyboard code

--- Envirosphere/ViewController.swift
+++ Envirosphere/ViewController.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var leading: NSLayoutConstraint!
     
     @IBOutlet weak var trailing: NSLayoutConstraint!
+    
+    @IBOutlet var table: UITableView!
+    var models = [FeedPost]()
+
     
     var menuOut = false
     
@@ -20,7 +24,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
+    
+    
+    table.register(PostTableViewCell.nib(), forCellReuseIdentifier: PostTableViewCell.identifier)
+    table.delegate = self
+    table.dataSource = self
+    
+    models.append(FeedPost(numberOfLikes: 200,
+                           username: "@envirosphere",
+                           userImageName: "envirospherelogo",
+                           postImageName: "enviroart"))
+  
+    models.append(FeedPost(numberOfLikes: 100,
+                           username: "@ecowarriors",
+                           userImageName: "bedroom",
+                           postImageName: "cow"))
+    
+    models.append(FeedPost(numberOfLikes: 200,
+                           username: "@pinterest",
+                           userImageName: "livingroom",
+                           postImageName: "posterart"))
+    
+    models.append(FeedPost(numberOfLikes: 400,
+                           username: "@ecoart",
+                           userImageName: "bathroom",
+                           postImageName: "goldfish"))
+}
 
     @IBAction func menuTapped(_ sender: Any) {
         
@@ -41,71 +70,30 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
 }
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+}
     
-    @IBOutlet var table: UITableView!
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier, for: indexPath) as! PostTableViewCell
+        cell.configure(with: models[indexPath.row])
+        return cell
+}
     
-    var models = [FeedPost]()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        table.register(PostTableViewCell.nib(), forCellReuseIdentifier: PostTableViewCell.identifier)
-        table.delegate = self
-        table.dataSource = self
-        
-        models.append(FeedPost(numberOfLikes: 200,
-                               username: "@envirosphere",
-                               userImageName: "envirospherelogo",
-                               postImageName: "enviroart"))
-      
-        models.append(FeedPost(numberOfLikes: 100,
-                               username: "@ecowarriors",
-                               userImageName: "bedroom",
-                               postImageName: "cow"))
-        
-        models.append(FeedPost(numberOfLikes: 200,
-                               username: "@pinterest",
-                               userImageName: "livingroom",
-                               postImageName: "posterart"))
-        
-        models.append(FeedPost(numberOfLikes: 400,
-                               username: "@ecoart",
-                               userImageName: "bathroom",
-                               postImageName: "goldfish"))
-    }
-    
-        func numberOfSections(in tableView: UITableView) -> Int {
-            return 1
-    }
-    
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return models.count
-    }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier, for: indexPath) as! PostTableViewCell
-            cell.configure(with: models[indexPath.row])
-            return cell
-    }
-        
-        func tableView(_ tableView: UITableView, didSelectRowAt: IndexPath) {
-        
-    }
+    func tableView(_ tableView: UITableView, didSelectRowAt: IndexPath) {
     
 }
+    struct FeedPost {
+        let numberOfLikes: Int
+        let username: String
+        let userImageName: String
+        let postImageName: String
+    }
 
-struct FeedPost {
-    let numberOfLikes: Int
-    let username: String
-    let userImageName: String
-    let postImageName: String
+
 }
-
-
-
+    
