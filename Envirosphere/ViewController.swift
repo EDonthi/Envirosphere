53bc6c465c88003f2e41fdead1911db790a1a0e5
Maiya Raghu <maiya.raghu@aol.com>
2021-07-22 13:32:46 -0400

    facts code

--- Envirosphere/ViewController.swift
+++ Envirosphere/ViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
+    @IBAction func funFactButton(_ sender: UIButton) {
+    }
+    
     @IBOutlet weak var leading: NSLayoutConstraint!
     
     @IBOutlet weak var trailing: NSLayoutConstraint!
